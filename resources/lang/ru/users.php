7c81d510ed81a00e701c2784ec5c08cfdc8d8d4c
FH <homidovfiruz23@gmail.com>
2025-09-23 17:09:00 +0500

    distribute_to_agents functions

---
+++ resources/lang/ru/users.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_rule' => 'Укажите distribute_to_agents=true для авто-распределения ИЛИ передайте agent_id.',
+    'self_transfer_forbidden' => 'Нельзя передать объекты самому удаляемому пользователю.',
+    'agent_required' => 'agent_id должен указывать на пользователя с ролью агент.',
+    'no_agents_to_distribute' => 'Нет доступных агентов для авто-распределения.',
+    'deleted_and_reassigned' => 'Пользователь удалён и объекты перераспределены.',
+    'password_incorrect' => 'Текущий пароль введён неверно.',
+    'photo_deleted' => 'Ваша фотография была удалена.',
+    'photo_updated' => 'Фотография обновлена.',
+];
---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'required' => 'Поле :attribute обязательно.',
+    'email' => 'Поле :attribute должно быть корректным email.',
+    'unique' => 'Такое значение поля :attribute уже существует.',
+    'min' => [
+        'string' => 'Поле :attribute должно содержать не менее :min символов.',
+    ],
+    'confirmed' => 'Поле :attribute не совпадает с подтверждением.',
+    'attributes' => [
+        'name' => 'Имя',
+        'phone' => 'Телефон',
+        'email' => 'Email',
+        'birthday' => 'Дата рождения',
+        'password' => 'Пароль',
+        'photo' => 'Фотография',
+        'agent_id' => 'Агент',
+    ],
+];
