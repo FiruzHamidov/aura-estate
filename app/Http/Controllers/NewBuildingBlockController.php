4036770c414dc7eac1b6689aa5f200a6018afeef
FH <homidovfiruz23@gmail.com>
2025-12-03 18:42:39 +0500

    new Building District

--- app/Http/Controllers/NewBuildingBlockController.php
+++ app/Http/Controllers/NewBuildingBlockController.php
@@ -9,31 +9,36 @@ use App\Http\Requests\UpdateNewBuildingBlockRequest;
 
 class NewBuildingBlockController extends Controller
 {
+    // GET /new-buildings/{new_building}/blocks
     public function index(NewBuilding $new_building)
     {
         return $new_building->blocks()->orderBy('name')->get();
     }
 
+    // POST /new-buildings/{new_building}/blocks
     public function store(StoreNewBuildingBlockRequest $request, NewBuilding $new_building)
     {
         $block = $new_building->blocks()->create($request->validated());
-        return response()->json($block, 201);
+        return response()->json($block->load(/* связи если нужно */), 201);
     }
 
-    public function show(NewBuildingBlock $block)
+    // GET /new-buildings/{new_building}/blocks/{block}
+    public function show(NewBuilding $new_building, NewBuildingBlock $block)
     {
-        return $block;
+        return $block->load(/* связи если нужно */);
     }
 
+    // PUT/PATCH /new-buildings/{new_building}/blocks/{block}
     public function update(UpdateNewBuildingBlockRequest $request, NewBuilding $new_building, NewBuildingBlock $block)
     {
         $block->update($request->validated());
         return $block->refresh();
     }
 
-    public function destroy(NewBuildingBlock $new_building_block)
+    // DELETE /new-buildings/{new_building}/blocks/{block}
+    public function destroy(NewBuilding $new_building, NewBuildingBlock $block)
     {
-        $new_building_block->delete();
+        $block->delete();
         return response()->noContent();
     }
 }
